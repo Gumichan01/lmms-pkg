73fdd8e9daa26aefde924bc56fb513171619af71
Gumichan01 <luxon.jean.pierre@gmail.com>
2021-01-14 23:27:14 +0100

    Get paths to sample files from the LMMS project file

---
+++ src/packager/packager.cpp
@@ -0,0 +1,83 @@
+/*
+*   LMMS Project Packager
+*   Copyright © 2021 Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+
+#include "packager.hpp"
+#include "../external/tinyxml2/tinyxml2.h"
+
+namespace Packager
+{
+
+bool contains( const std::vector<std::string> names, const std::string& s )
+{
+    return std::find( names.cbegin(), names.cend(), s ) != names.cend();
+}
+
+const std::vector<tinyxml2::XMLElement *> getAllElementsByNames( tinyxml2::XMLElement * root, const std::vector<std::string>& names )
+{
+    std::vector<tinyxml2::XMLElement *> retrieved_elements;
+    tinyxml2::XMLElement * element = root->FirstChildElement();
+
+    while ( element != nullptr )
+    {
+        if ( contains( names, element->Name() ) )
+        {
+            retrieved_elements.push_back( element );
+        }
+        const std::vector<tinyxml2::XMLElement *> elements = getAllElementsByNames( element, names );
+        retrieved_elements.insert( retrieved_elements.end(), elements.cbegin(), elements.cend() );
+        element = element->NextSiblingElement();
+    }
+
+    return retrieved_elements;
+}
+
+const std::vector<std::string> retrievePathsOfFilesFromXMLFile( const std::string& project_file )
+{
+    tinyxml2::XMLDocument doc;
+    tinyxml2::XMLError code = doc.LoadFile( project_file.c_str() );
+
+    if ( code != tinyxml2::XML_SUCCESS )
+    {
+        std::cerr << "Ouch! Tinyxml Error code: " << code << "\n";
+        return std::vector<std::string>();
+    }
+
+    tinyxml2::XMLElement * root = doc.RootElement();
+
+    if ( root == nullptr )
+    {
+        std::cerr << "No root element. Are you sure this file contains an XML content?\n";
+        return std::vector<std::string>();
+    }
+
+    const std::vector<std::string> NAMES{ "audiofileprocessor", "sf2player", "sampletco" };
+    const std::vector<tinyxml2::XMLElement *> elements = getAllElementsByNames( root, NAMES );
+
+    std::unordered_set<std::string> unique_paths;
+    std::for_each( elements.cbegin(), elements.cend(), [&unique_paths]( tinyxml2::XMLElement * e )
+    {
+        unique_paths.insert( e->Attribute( "src" ) );
+    } );
+
+    return std::vector<std::string>( unique_paths.cbegin(), unique_paths.cend() );
+}
+}
