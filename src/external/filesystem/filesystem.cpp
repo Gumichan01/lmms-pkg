5da6110aa28f1ec94d4cfaa70dfbdca2bcaa325e
Gumichan01 <luxon.jean.pierre@gmail.com>
2021-01-16 16:30:42 +0100

    Add an external filesystem library
    This built-in library was imported from LunatiX and adapted fore this project

---
+++ src/external/filesystem/filesystem.cpp
@@ -0,0 +1,217 @@
+
+/*
+*   LMMS Project Packager
+*   Copyright © 2021 Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// This file was taken from LunatiX, and has been adapted for this project
+
+#include <algorithm>
+
+#include "filesystem.hpp"
+
+namespace fs
+{
+
+#if defined(__WIN32__) || defined(__WIN64__)
+const char * CURRENT_DIR = ".\\";
+const UTF8string SEPARATOR ( "\\" );
+#else
+const char * CURRENT_DIR = "./";
+const UTF8string SEPARATOR ( "/" );
+#endif
+
+const UTF8string CURRENT ( "." );
+const UTF8string PARENT ( ".." );
+const UTF8string ROOT ( SEPARATOR );
+const UTF8string SCURS ( SEPARATOR + CURRENT + SEPARATOR );
+const UTF8string SPARS ( SEPARATOR + PARENT  + SEPARATOR );
+
+UTF8string removeTrailingSep ( const UTF8string& u8str ) noexcept;
+size_t countSeparator ( const UTF8string& u8str ) noexcept;
+bool checkBasename ( const UTF8string& npath );
+bool checkDirname ( const UTF8string& npath );
+UTF8string utf8Basename ( const UTF8string& path ) noexcept;
+UTF8string utf8Dirname ( const UTF8string& path ) noexcept;
+
+// separator: '/' on POSIX system, '\' on Windows
+UTF8string removeTrailingSep ( const UTF8string& u8str ) noexcept
+{
+    const UTF8iterator u8end = u8str.utf8_end();
+    UTF8iterator it = u8str.utf8_begin();
+    UTF8string u8s;
+
+    while ( it != u8end )
+    {
+        u8s += *it;
+
+        if ( *it == SEPARATOR )
+        {
+            UTF8iterator tmp_it = it + 1;
+            while ( tmp_it != u8end && *tmp_it == SEPARATOR )
+            {
+                ++tmp_it;
+            }
+
+            it = tmp_it;
+        }
+        else
+            ++it;
+    }
+
+    return u8s;
+}
+
+size_t countSeparator ( const UTF8string& u8str ) noexcept
+{
+    const char SEP = SEPARATOR.utf8_sstring()[0];
+    const std::string& u8string = u8str.utf8_sstring();
+    return static_cast<size_t>( std::count( u8string.begin(), u8string.end(), SEP ) );
+}
+
+bool checkBasename ( const UTF8string& npath )
+{
+    // ".", ".." or root directory → return the path
+    // No separator → the path itself
+    return npath == CURRENT || npath == PARENT || npath == ROOT
+           || npath.utf8_find ( SEPARATOR ) == UTF8string::npos;
+}
+
+bool checkDirname ( const UTF8string& npath )
+{
+    // Empty string
+    if ( npath.utf8_empty() )
+        return true;
+
+    const size_t POS = npath.utf8_find ( SEPARATOR );
+    const size_t U8LEN = npath.utf8_length();
+
+    // Current directory or parent → current
+    // "." or ".." between separators → current directory
+    // OR No separator → CURRENT directory
+    // OR The path contains at least one separator
+    // and the first separator is at the end of string → it is unique
+    return npath == CURRENT || npath == PARENT || npath == SCURS
+           || npath == SPARS || POS == UTF8string::npos || POS == U8LEN - 1;
+}
+
+
+UTF8string utf8Basename ( const UTF8string& path ) noexcept
+{
+    const UTF8string npath = removeTrailingSep ( path );
+
+    // Empty string → current
+    if ( npath.utf8_empty() )
+        return CURRENT;
+
+    // basic checking
+    if ( checkBasename ( npath ) )
+        return npath;
+
+    // The path contains at least one separator
+    const size_t U8LEN = npath.utf8_length();
+    const UTF8iterator beg = npath.utf8_begin();
+    UTF8iterator it = -- ( npath.utf8_end() );
+    size_t spos = 0;
+    bool end_sep = false;
+
+    // If the last character is a separator ('/', '\')
+    if ( *it == SEPARATOR )
+    {
+        end_sep = true;
+        spos = U8LEN - 1;
+        --it;
+    }
+    else
+        spos = U8LEN;
+
+    while ( *it != SEPARATOR && it != beg )
+    {
+        --it;
+        --spos;
+    }
+
+    // If there is a separator at the beginning
+    if ( *it == SEPARATOR )
+    {
+        if ( end_sep )
+            return npath.utf8_substr ( spos, U8LEN - spos - 1 );
+        else
+            return npath.utf8_substr ( spos, U8LEN );
+    }
+
+    return npath.utf8_substr ( 0, U8LEN - 1 );
+}
+
+UTF8string utf8Dirname ( const UTF8string& path ) noexcept
+{
+    const UTF8string npath = removeTrailingSep ( path );
+
+    // root directory → return the path
+    if ( npath == ROOT )
+        return npath;
+
+    // Other particular cases
+    if ( checkDirname ( npath ) )
+        return CURRENT;
+
+    const size_t POS = npath.utf8_find ( SEPARATOR );
+    const size_t U8LEN = npath.utf8_length();
+
+    // If a unique separator was found and is at position 0 → root
+    if ( POS == 0 && countSeparator ( npath ) == 1 )
+    {
+        return ROOT;
+    }
+    else if ( countSeparator ( npath ) == 1 )
+    {
+        return npath.utf8_substr ( 0, POS + 1 );
+    }
+
+    // At this point, there are more than 1 separators
+    const UTF8iterator beg = npath.utf8_begin();
+    UTF8iterator it = -- ( npath.utf8_end() );
+    size_t spos = U8LEN - 1;
+
+    // Last character == separator → do not count it
+    if ( *it == SEPARATOR )
+    {
+        --it;
+        --spos;
+    }
+
+    while ( it != beg && *it != SEPARATOR )
+    {
+        --it;
+        --spos;
+    }
+
+    return npath.utf8_substr ( 0, ( spos == 0 ? spos + 1 : spos ) );
+}
+
+// Public
+
+std::string basename( const std::string& path ) noexcept
+{
+    return utf8Basename(path).utf8_sstring();
+}
+std::string dirname( const std::string& path ) noexcept
+{
+    return utf8Dirname(path).utf8_sstring();
+}
+
+}   // fs
+
---
+++ src/external/filesystem/filesystem.hpp
@@ -0,0 +1,37 @@
+
+/*
+*   LMMS Project Packager
+*   Copyright © 2021 Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// This file was taken from LunatiX, and has been adapted for this project
+
+#ifndef FILESYSTEM_HPP_INCLUDED
+#define FILESYSTEM_HPP_INCLUDED
+
+#include "../utf8/utf8_string.hpp"
+#include <string>
+
+namespace fs
+{
+
+// Note: Trailing '\' (on Windows) or '/' (on Linux) are not counted as part of the path name.
+std::string basename( const std::string& path ) noexcept;
+std::string dirname( const std::string& path ) noexcept;
+
+}
+
+#endif // FILESYSTEM_HPP_INCLUDED
