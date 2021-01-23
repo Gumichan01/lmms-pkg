9e6c32313e971056c5939626d877bf5affa6cd4f
Gumichan01 <luxon.jean.pierre@gmail.com>
2021-01-23 21:08:12 +0100

    Implement basic program options

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,46 @@
 /*
 *   LMMS Project Packager
-*	Copyright © 2021 Luxon Jean-Pierre
+*   Copyright © 2021 Luxon Jean-Pierre
 *
-*	This program is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
 *
-*	This program is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*	GNU General Public License for more details.
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
 *
-*	You should have received a copy of the GNU General Public License
-*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <iostream>
 #include <cstdlib>
+#include <algorithm>
 
 #include "packager/packager.hpp"
+#include "packager/options.hpp"
+#include "external/filesystem/filesystem.hpp"
 
-int main(int argc, char * argv[]) {
+int main( int argc, char * argv[] )
+{
+    const int MINIMUM_ARGC = 4;
 
-    const std::string PACKAGE_PATH = "export";
-    const std::vector<std::string>& v = Packager::retrievePathsOfFilesFromXMLFile("D:/git_dir/lmms-pkg/data/demo-evasion.mmp");
+    if ( argc < MINIMUM_ARGC )
+    {
+        std::cerr << "Invalid number of arguments\n"
+                  << "usage: " << fs::basename( argv[0] )
+                  << " --import|--export [--no-sf2] [--lmms-data <path/to/lmms/data>] <name>.mmp(z) <destination-path> \n"
+                  << "/!\\ Some arguments are not active yet \n\n";
+        return EXIT_FAILURE;
+    }
+
+    const options::Options& options = options::retrieveArguments( argc, argv );
+    const std::vector<std::string>& v = Packager::retrievePathsOfFilesFromXMLFile( options.project_file );
     std::cout << "Number of files: " << v.size() << " \n";
-    const std::vector<std::string>& v2 = Packager::copyFilesTo(v, PACKAGE_PATH);
+    const std::vector<std::string>& v2 = Packager::copyFilesTo( v, options.destination_path );
     std::cout << "Copied files: " << v2.size() << " \n";
     return EXIT_SUCCESS;
 }
---
+++ src/packager/options.cpp
@@ -0,0 +1,46 @@
+/*
+*   LMMS Project Packager
+*   Copyright © 2021 Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "options.hpp"
+#include "../external/filesystem/filesystem.hpp"
+
+namespace options
+{
+
+const Options retrieveArguments( int argc, char * argv[] ) noexcept
+{
+    // Assuming there are at least 4 arguments
+    const std::string& project_file = fs::normalize( argv[argc - 2] );
+    const std::string& destination_path = fs::normalize( argv[argc - 1] );
+
+    const std::string& operation_str = argv[1];
+    OperationType op;
+
+    if ( operation_str == "--import" )
+    {
+        op = OperationType::Import;
+    }
+    else if ( operation_str == "--export" )
+    {
+        op = OperationType::Export;
+    }
+
+    return Options{ op, project_file, destination_path };
+}
+
+}
