1ca613debc8884757cf566c16b72a7726983519a
Gumichan01 <luxon.jean.pierre@gmail.com>
2021-02-07 00:07:22 +0100

    Implement compression of the package on Windows with Zip Utils
    The code must be portable on Windows and Linux, so the external dependencies
    must be easily used in those platforms. However, I wasted my time trying to compile
    libzippp (this library is multi-platform) on Windows.
    
    Maybe I was doing it wrong bu honestly I don't care, so on Linux I uses libzippp
    (it is so easy to get things done on alinux distribtution) and on Windows I use
    Zip Utils. I have two zip libraries to maintain inside the packager but I can handle it.
    
    Fuck Windows!
    
    Link:
    - http://www.wischik.com/lu/programmer/zip_utils.html

--- src/external/lmms-zip/mmpz.cpp
+++ src/external/lmms-zip/mmpz.cpp
@@ -16,20 +16,28 @@
 *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 
 #include "mmpz.hpp"
 #include "../filesystem/filesystem.hpp"
-#include "../libzip/libzippp.h"
 #include "../ghc/filesystem.hpp"
 
+#if defined(__WIN32__) || defined(__WIN64__)
+
+#include "../zutils-win32/zutils.hpp"
+
+#else
+
+#include "../libzip/libzippp.h"
 using namespace libzippp;
+
+#endif
+
 namespace fsys = ghc::filesystem;
 
 namespace lmms
 {
+
 
 std::string decompressProject( const std::string& project_file, const std::string& package_directory,
                                const std::string& lmms_command ) noexcept
@@ -55,6 +63,62 @@ std::string decompressProject( const std::string& project_file, const std::strin
     return xml_file;
 }
 
+bool compressPackage( const std::string& package_directory, const std::string& package_name ) noexcept;
+
+bool compressPackage( const std::string& package_directory, const std::string& package_name ) noexcept
+{
+
+// I could not compile this fucking libzippp on Windows because of those dammned dependencies.
+// I don't want to waste my time installing VStudio, I am a FLOSS advocate ( -.-).
+#if defined(__WIN32__) || defined(__WIN64__)
+
+    HZIP zip = CreateZip( package_name.c_str(), nullptr );
+
+    for ( auto& file : fsys::recursive_directory_iterator( package_directory ) )
+    {
+        if ( fsys::is_regular_file( file.path() ) )
+        {
+            std::cout << "zip: " << file.path().string() << "\n";
+            ZipAdd( zip, file.path().string().c_str(), file.path().string().c_str() );
+        }
+        else
+        {
+            std::cout << file.path().string() << " is something else\n";
+        }
+    }
+
+    CloseZip( zip );
+#else
+
+    ZipArchive zf( package_name );
+    zf.open( ZipArchive::Write );
+
+    if ( zf.isOpen() )
+    {
+        for ( auto& file : fsys::recursive_directory_iterator( package_directory ) )
+        {
+            if ( fsys::is_regular_file( file.path() ) )
+            {
+                std::cout << "zip: " << file.path().string() << "\n";
+                zf.addFile( file.path().string(), file.path().string() );
+            }
+            else
+            {
+                std::cout << file.path().string() << " is something else\n";
+            }
+        }
+    }
+    else
+    {
+        std::cerr << "-- ERROR: Cannot zip " << package_name << ".\n";
+        return false;
+    }
+    zf.close();
+
+#endif
+    return true;
+}
+
 std::string zipFile( const std::string& package_directory ) noexcept
 {
     if ( package_directory.empty() )
@@ -73,33 +137,7 @@ std::string zipFile( const std::string& package_directory ) noexcept
         package_name = package_directory + ".zip";
     }
 
-    ZipArchive zf(package_name);
-    zf.open(ZipArchive::Write);
-
-    if (zf.isOpen())
-    {
-        std::cout << "OK\n";
-        for (auto &file : fsys::recursive_directory_iterator(package_directory))
-        {
-            if (fsys::is_regular_file(file.path()))
-            {
-                std::cout << "zip: " << file.path().string() << "\n";
-                zf.addFile( file.path().string(), file.path().string() );
-            }
-            else
-            {
-                std::cout << file.path().string() << " is something else\n";
-            }
-        }
-    }
-    else
-    {
-        std::cerr << "-- ERROR: Cannot zip " << package_name << ".\n";
-        return "";
-    }
-    zf.close();
-
-    return package_name;
+    return compressPackage( package_directory, package_name ) ? package_name : "";
 }
 
 }
---
+++ src/external/zutils-win32/zutils.hpp
@@ -0,0 +1,26 @@
+
+/*
+*   LMMS Project Packager
+*   Copyright © 2021 Luxon Jean-Pierre
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// This Zutils header is the entry point of zip.h and unzip.h
+// Use it if you are compiling the script on Windows
+
+// See http://www.wischik.com/lu/programmer/zip_utils.html
+
+#include "zip.h"
+#include "unzip.h"
